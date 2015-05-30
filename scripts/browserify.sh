014269dcc7742a99d87a7dd6b8a8a95b2c07ff89
Doug Chimento <dchimento@gmail.com>
2015-05-30 09:46:34 -0400

    adding discify

--- scripts/browserify.sh
+++ scripts/browserify.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./node_modules/.bin/browserify -t reactify --full-paths  -o webapp/js/bundle.js  `find webapp/jsx/ -name '*.jsx'`
+./node_modules/.bin/browserify --full-paths -t reactify -o webapp/js/bundle.js  `find webapp/jsx/ -name '*.js*'` `find webapp/content/ -name '*.js*'` `find webapp/lib/ -name '*.js*'`
+
