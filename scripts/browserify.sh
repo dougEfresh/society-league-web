4a7c4baa880d17ed36641b7bc8051c097b14f7db
Doug Chimento <dchimento@gmail.com>
2015-05-30 12:33:16 -0400

    Adding just find webapp/ -name '*.jsx to browserfy

--- scripts/browserify.sh
+++ scripts/browserify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./node_modules/.bin/browserify --full-paths -t reactify -o webapp/js/bundle.js  `find webapp/jsx/ -name '*.js*'` `find webapp/content/ -name '*.js*'` `find webapp/lib/ -name '*.js*'`
+./node_modules/.bin/browserify --full-paths -t reactify -o webapp/js/bundle.js  `find webapp/ -name '*.jsx'` 
 
