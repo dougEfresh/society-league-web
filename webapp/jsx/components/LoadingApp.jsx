dc572b3e7efbeba2aec87feabf835249eeb9db64
Doug Chimento <dchimento@gmail.com>
2015-06-12 16:11:15 -0400

    fixing class=

--- webapp/jsx/components/LoadingApp.jsx
+++ webapp/jsx/components/LoadingApp.jsx
@@ -4,9 +4,9 @@ var Panel = BootStrap.Panel;
 var LoadingApp = React.createClass({
     render: function () {
         return (<div id="loadingApp">
-             <div class="form-group col-lg-12">
-                        <div class="text-center">
-                            <span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>
+             <div className="form-group col-lg-12">
+                        <div className="text-center">
+                            <span className="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>
                         </div>
                     </div>
         </div>);
