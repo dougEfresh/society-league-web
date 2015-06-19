287ce8de8cd3ea456b81e431dff0d235b4b9deaf
Bob Smith <bob.smith@societybilliards.com>
2015-06-19 18:26:33 -0400

    Puting Link outside of button

--- webapp/content/scout/StatApp.jsx
+++ webapp/content/scout/StatApp.jsx
@@ -36,16 +36,17 @@ var StatApp = React.createClass({
         }
         var header = (
                 <div className="btn-group bot-margin">
+                    <Link to='stats' params={this.getParams()}>
                         <button className={this.isActive('stats') ? 'btn btn-success' : 'btn btn-default'}>
-                            <Link to='stats' params={this.getParams()}>
-                                <span className="fa fa-bar-chart"></span><span className="main-item">{ ' Stats'}</span>
-                            </Link>
+                            <span className="fa fa-bar-chart"></span>
+                            <span className="main-item">Stats</span>&nbsp;
                         </button>
+                    </Link>
+                    <Link to='history' params={this.getParams()}>
                         <button className={this.isActive('history') ? 'btn btn-success' : 'btn btn-default'}>
-                            <Link to='history' params={this.getParams()}>
-                                <span className="fa fa-history"></span><span className="main-item">{ ' History'}</span>
-                            </Link>
+                            <span className="fa fa-history"></span><span className="main-item"> History</span>&nbsp;
                         </button>
+                    </Link>
                 </div>
         );
         //<h3><span className="fa fa-bar-chart"></span>Stats</h3>
