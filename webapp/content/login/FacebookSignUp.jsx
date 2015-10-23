f57658183151f1b291f2e23f554e608a538a6778
Douglas Chimento <douglas.chimento@yodle.com>
2015-10-23 11:36:09 -0400

    changing submit button

--- webapp/content/login/FacebookSignUp.jsx
+++ webapp/content/login/FacebookSignUp.jsx
@@ -15,29 +15,7 @@ var LoginApp = React.createClass({
             error: false,
             loggedIn: false
         };
-    },
-    handleSubmitFB: function(e){
-        e.preventDefault();
-        $.ajax({
-            async: true,
-            processData: true,
-            url: '/connect/facebook',
-            method: 'post',
-            data: {scope: 'public_profile,email,user_friends'},
-            success: function (d) {
-                DataStore.setUser(d);
-                if (this.context.location.pathname == "/" || this.context.location.pathname == "/login") {
-                    this.history.replaceState(null, '/app/home');
-                    return;
-                }
-                this.history.replaceState(null, this.context.location.pathname,this.context.location.query);
-            }.bind(this),
-            error: function (xhr, status, err) {
-                console.error('authenticate', status, err.toString());
-                this.history.pushState(null, '/login', {error: 'true'});
-            }.bind(this)
-        });
-    },
+    },   
     render: function () {
         var errorMsg = null;
         if (this.context.location.query.error == 'true') {
@@ -57,9 +35,8 @@ var LoginApp = React.createClass({
                         </div>
                         <div className="row">
                             <div className="btn-group col-lg-6 col-md-7 col-sm-12 col-xs-12 login-options">
-                                 <button className="btn btn-block btn-social btn-facebook" type="submit">
-                                     <i className="fa fa-facebook"></i>
-                                     Sign in with Facebook
+                                 <button  className="btn btn-sm btn-primary btn-responsive" type="submit">
+                                     Submit
                                  </button>
                             </div>
                         </div>
